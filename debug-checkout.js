e273cfa2f95328e599b5e5a0b336cd6a268f49ee
Jacques Artgraven <jacques@sinappsus.agency>
2025-09-08 13:48:51 +0200

    remove debug file

--- debug-checkout.js
+++
@@ -1,8 +0,0 @@
-// Add this to your browser console on the checkout page to check
-console.log('Checkout type check:');
-console.log('Has .wp-block-woocommerce-checkout:', document.querySelector('.wp-block-woocommerce-checkout') ? 'BLOCK CHECKOUT' : 'NOT FOUND');
-console.log('Has [woocommerce_checkout]:', document.querySelector('form.checkout') ? 'CLASSIC CHECKOUT' : 'NOT FOUND');
-console.log('Available payment gateways from WC blocks:', window.wc?.wcSettings?.getSetting('paymentMethodTypes'));
-console.log('All WC settings:', window.wc?.wcSettings?.allSettings);
-console.log('Payment method data:', window.wc?.wcSettings?.getSetting('paymentMethodData'));
-console.log('All available payment gateways:', window.wc?.wcSettings?.getSetting('paymentGateways'));
